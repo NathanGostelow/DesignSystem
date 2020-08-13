971b8fa89be926611fa09486ef7db57536441696
NathanGostelow <nathan-gostelow-94@hotmail.com>
2020-08-13 16:37:45 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    plugins: ['react'],
+    extends: ['eslint:all', 'plugin:react/all', 'airbnb']
+};
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './app.module.css';
+
+const StarterApp = () => {
+    return (
+        <div className="main">
+            <div className="container">
+                <h1> StarterApp </h1>
+            </div>
+        </div>
+    );
+};
+
+export default StarterApp;
